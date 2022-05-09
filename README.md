bc0c9dc7f6cddd727d13a0d015966227da1f954b
Milynda Moore <85596656+MoxieMils@users.noreply.github.com>
2022-05-08 17:15:33 -0700

    Initial Commit
    Migrate of Google Doodle - Web2 project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# GoogleDoodle
-Web 2 Google Doodle
+# My Topic Google Doodle
+
+50+ words about your Google Doodle.
+
+## Collage
+
+[Sample to Figma Collage](https://www.figma.com/file/RtNPxNM4F8s2r8Eldu1lfT/Google-Doodle?node-id=0%3A1)
+
+## Resources
---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// GSAP included in `index.html`
+console.log('OK');
+
+
+gsap.from('#image0_0_1', { duration: 2.5, ease: "bounce", y: -500 });
+
+gsap.from('#image1_0_1', { duration: 2.5, ease: "bounce", y: 500 });
+
+gsap.from('#image2_0_1', { duration: 2.5, ease: "bounce", y: 500 });
+          
+gsap.from('#image3_0_1', { duration: 2.5, ease: "bounce", y: -500 });
+
